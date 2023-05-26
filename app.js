a61c79fdec0a1db42aba587e9bdc72b7fc5e780d
CMLTTN <cemaldim@gmail.com>
2023-05-26 15:34:37 +0300

    math

--- app.js
+++ app.js
@@ -1,13 +1,7 @@
-const num1 = Math.ceil(Math.random()*10)
-console.log(num1);
-
-const num2 = Math.ceil(Math.random()*10)
-
-const questionEl = document.getElementById("question")
-
-
-const input = document.getElementById("input")
-
-question
-
-const correctAns = num1*num2
+ const num1 = Math.ceil(Math.random() * 10);
+ // console.log(num1);
+ const num2 = Math.ceil(Math.random() * 10);
+ const questionEl = document.getElementById("question");
+ const inputEl = document.getElementById("input");
+ questionEl.innerText = ` `;
+ const correctAns = num1 * num2;
