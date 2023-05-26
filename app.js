b307080d3c254cff2cf85e5a7886cf154d6c2fa6
CMLTTN <cemaldim@gmail.com>
2023-05-26 22:19:28 +0300

    done

--- app.js
+++ app.js
@@ -1,42 +1,35 @@
- const num1 = Math.ceil(Math.random() * 10);
+ const num1 = Math.ceil(Math.random() * 10); // 1. aşama
  // console.log(num1);
- const num2 = Math.ceil(Math.random() * 10);
-
- const questionEl = document.getElementById("question");
-
- const formEl =document.getElementById("form");
- const scoreEl = document.getElementById("score")
- let score = JSON.parse(localStorage.getItem("score"))
-
- if(!score){
-     score =0
+ const num2 = Math.ceil(Math.random() * 10); // 2. aşama
+ const questionEl = document.getElementById("question"); // 3. aşama
+ const inputEl = document.getElementById("input"); // 7. aşama
+ const formEl = document.getElementById("form"); // 6.aşama
+ const scoreEl = document.getElementById("score"); // 14. aşama
+ let score = JSON.parse(localStorage.getItem("score")); // 16. aşama
+ if (!score) {
+   // 17. aşama- yenileme oldugunda null olmamamsı için
+   score = 0;
  }
- scoreEl.innerText= `score:${score}`
-
-//  let score = 0;
-
- const inputEl = document.getElementById("input");
-
- questionEl.innerText = `What is ${num1}multiply by ${num2} `;
-
- const correctAns = num1 * num2;
-
-
- formEl.addEventListener("submit",()=>{
-const userAns =  +inputEl.value
-// console.log(userAns, typeof userAns);
-if (userAns ==correctAns){
-     score++
+ scoreEl.innerText = `score:${score}`; // 15. aşama
+ // let score = 0;  //11. aşama
+ questionEl.innerText = `What is ${num1} multiply by ${num2}`; // 4. aşama ve backtick kullan
+ const correctAns = num1 * num2; // 5. aşama
+ formEl.addEventListener("submit", () => {
+   // 6. aşama part 2
+   const userAns = +inputEl.value; // 8. aşama
+   // console.log(userAns, typeof userAns);   // 9. aşama
+   if (userAns === correctAns) {
+     // 10. aşama
+     score++;
      console.log(score);
-     updateLocalStorage()
-}else{
-     score--
+     updateLocalStorage(); // 13.aşama part 1
+   } else {
+     score--;
      console.log(score);
-     updateLocalStorage();
-
-}
- })
-
- function updateLocalStorage(){
-     localStorage.setItem("score", JSON.stringify(score))
+     updateLocalStorage(); // 13. aşama part 2
+   }
+ });
+ function updateLocalStorage() {
+   // 12. aşama
+   localStorage.setItem("score", JSON.stringify(score));
  }
