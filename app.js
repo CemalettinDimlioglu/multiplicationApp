a969593a71d70324708e7a9084fc179f05481900
CMLTTN <cemaldim@gmail.com>
2023-05-26 21:34:52 +0300

    add

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
  const num1 = Math.ceil(Math.random() * 10);
  // console.log(num1);
  const num2 = Math.ceil(Math.random() * 10);
+
  const questionEl = document.getElementById("question");
+
+ const formEl =document.getElementById("form");
+
  const inputEl = document.getElementById("input");
- questionEl.innerText = ` `;
- const correctAns = num1 * num2;
+
+ questionEl.innerText = `What is ${num1}multiply by ${num2} `;
+
+ const correctAns = num1 * num2; 
+
+
+ formEl.addEventListener("submit",()=>{
+const userAns =  +inputEl.value
+console.log(userAns, typeof userAns);
+ })
